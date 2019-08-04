109a4dcb04e1ea99afd071017067c12469dd171d
Anup Kumar Maharjan <anupkumarmaharjan@gmail.com>
2019-08-04 06:39:40 +0545

    themeing

--- resources/views/frontend/partials/search.blade.php
+++ resources/views/frontend/partials/search.blade.php
@@ -1,67 +1,63 @@
-
 <!-- SEARCH SECTION -->
+<section class="indigo darken-2 white-text center search-bar">
+	<div class="search-bar__fixed">
+		<div class="container">
+			<div class="row m-b-0">
+				<div class="col s12">
+					<form action="{{ route('search')}} " method="GET">
+						<div class="searchbar">
+							<div class="input-field col s12 m3">
+								<input type="text" name="city" id="autocomplete-input" class="autocomplete custominputbox" autocomplete="off">
+								<label for="autocomplete-input">Enter City or State</label>
+							</div>
 
-<section class="indigo darken-2 white-text center">
-    <div class="container">
-        <div class="row m-b-0">
-            <div class="col s12">
+							<div class="input-field col s12 m2">
+								<select name="type" class="browser-default">
+									<option value="" disabled selected>Choose Type</option>
+									<option value="bungalow">Bungalow</option>
+									<option value="house">House</option>
+									<option value="land">Land</option>
+									<option value="rent">Rent</option>
+									<option value="apartment">Apartment</option>
+									<option value="colony">Colony</option>
+									<option value="flat">Flat</option>
+								</select>
+							</div>
 
-                <form action="{{ route('search')}} " method="GET">
+							<div class="input-field col s12 m2">
+								<select name="purpose" class="browser-default">
+									<option value="" disabled selected>Purpose</option>
+									<option value="rent">Rent</option>
+									<option value="sale">Sale</option>
+									<option value="lease">Lease</option>
+								</select>
+							</div>
 
-                    <div class="searchbar">
-                        <div class="input-field col s12 m3">
-                            <input type="text" name="city" id="autocomplete-input" class="autocomplete custominputbox" autocomplete="off">
-                            <label for="autocomplete-input">Enter City or State</label>
-                        </div>
+							<div class="input-field col s12 m2">
+								<select name="bedroom" class="browser-default">
+									<option value="" disabled selected>Bedroom</option>
+									@if(isset($bedroomdistinct))
+										@foreach($bedroomdistinct as $bedroom)
+											<option value="{{$bedroom->bedroom}}">{{$bedroom->bedroom}}</option>
+										@endforeach
+									@endif
+								</select>
+							</div>
 
-                        <div class="input-field col s12 m2">
-                            <select name="type" class="browser-default">
-                                <option value="" disabled selected>Choose Type</option>
-                                <option value="bungalow">Bungalow</option>
-                                <option value="house">House</option>
-                                <option value="land">Land</option>
-                                <option value="rent">Rent</option>
-                                <option value="apartment">Apartment</option>
-                                <option value="colony">Colony</option>
-                                <option value="flat">Flat</option>
-                            </select>
-                        </div>
+							<div class="input-field col s12 m2">
+								<input type="text" name="maxprice" id="maxprice" class="custominputbox">
+								<label for="maxprice">Max Price</label>
+							</div>
 
-                        <div class="input-field col s12 m2">
-                            <select name="purpose" class="browser-default">
-                                <option value="" disabled selected>Purpose</option>
-                                <option value="rent">Rent</option>
-                                <option value="sale">Sale</option>
-                                <option value="lease">Lease</option>
-                            </select>
-                        </div>
-
-                        <div class="input-field col s12 m2">
-                            <select name="bedroom" class="browser-default">
-                                <option value="" disabled selected>Bedroom</option>
-                                @if(isset($bedroomdistinct))
-                                    @foreach($bedroomdistinct as $bedroom)
-                                        <option value="{{$bedroom->bedroom}}">{{$bedroom->bedroom}}</option>
-                                    @endforeach
-                                @endif
-                            </select>
-                        </div>
-
-                        <div class="input-field col s12 m2">
-                            <input type="text" name="maxprice" id="maxprice" class="custominputbox">
-                            <label for="maxprice">Max Price</label>
-                        </div>
-                        
-                        <div class="input-field col s12 m1">
-                            <button class="btn btnsearch waves-effect waves-light w100" type="submit">
-                                <i class="material-icons">search</i>
-                            </button>
-                        </div>
-                    </div>
-
-                </form>
-
-            </div>
-        </div>
-    </div>
+							<div class="input-field col s12 m1">
+								<button class="btn btnsearch waves-effect waves-light w100" type="submit">
+									<i class="material-icons">search</i>
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
