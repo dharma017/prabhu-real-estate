3a98d03a01ba32c3f12148b2302c279f2dd9334a
Dharma Thapa <thapa.dharma017@gmail.com>
2019-08-05 10:54:41 +0545

    Update mobile menu

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -125,6 +125,51 @@
         <li class="{{ Request::is('contact') ? 'active' : '' }}">
             <a href="{{ route('contact') }}">Contact Us</a>
         </li>
+
+        @guest
+            <li><a href="{{ route('login') }}"><i class="material-icons">input</i></a></li>
+            <li><a href="{{ route('register') }}"><i class="material-icons">person_add</i></a></li>
+        @else
+            <li>
+                <a class="dropdown-trigger" href="#!" data-target="dropdown-auth-frontend_mobile">
+                    {{ ucfirst(Auth::user()->username) }}
+                    <i class="material-icons right">arrow_drop_down</i>
+                </a>
+            </li>
+
+            <ul id="dropdown-auth-frontend_mobile" class="dropdown-content">
+                <li>
+                    @if(Auth::user()->role->id == 1)
+                        <a href="{{ route('admin.dashboard') }}" class="indigo-text">
+                            <i class="material-icons">person</i>Profile
+                        </a>
+                        <a href="{{ route('agents') }}" class="indigo-text">
+                            <i class="material-icons">people</i>Agents
+                        </a>
+                    @elseif(Auth::user()->role->id == 2)
+                        <a href="{{ route('agent.dashboard') }}" class="indigo-text">
+                            <i class="material-icons">person</i>Profile
+                        </a>
+                    @elseif(Auth::user()->role->id == 3)
+                        <a href="{{ route('user.dashboard') }}" class="indigo-text">
+                            <i class="material-icons">person</i>Profile
+                        </a>
+                    @endif
+                </li>
+                <li>
+                    <a class="dropdownitem indigo-text" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        <i class="material-icons">power_settings_new</i>{{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+
+        @endguest
     </ul>
 
 </div>
