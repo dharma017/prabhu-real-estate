05e14ba232eb45d6b602f17575e7c3ddd7b9f18c
Anup Kumar Maharjan <anupkumarmaharjan@gmail.com>
2019-08-19 22:30:51 +0545

    swiper used for slider

--- resources/views/pages/properties/slider.blade.php
+++ resources/views/pages/properties/slider.blade.php
@@ -70,9 +70,9 @@
 <script>
 	var galleryThumbs = new Swiper('.gallery-thumbs', {
 		spaceBetween: 10,
-		slidesPerView: 4,
+		slidesPerView: 1,
 		loop: true,
-		freeMode: true,
+		// freeMode: true,
 		loopedSlides: 5, //looped slides should be the same
 		watchSlidesVisibility: true,
 		watchSlidesProgress: true,
